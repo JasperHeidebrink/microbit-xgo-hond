cf7db14315832b6ca25bf7d8741b50f2de03f456
Jasper <jasper@dragonet.nl>
2026-07-16 10:11:35 +0200

    basic movement

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+let moveY = 0
+let moveX = 0
+radio.setGroup(25)
 basic.forever(function () {
-	
+    radio.onReceivedValue(function( name, value) {
+        basic.showString(name+':'+value)
+        if(name=='rockerX'){
+            moveX = value;
+
+            xgo.move_xgo(xgo.direction_enum.Forward, value)
+        } else if(name == 'rockerY'){
+            moveY = value;
+            xgo.move_xgo(xgo.direction_enum.Left, value)
+        }
+
+    });
 })
